464216488a0b59b692c0464966932746dfe0fa4d
jeffin-abraham <jeffin-abraham@users.noreply.github.com>
2020-07-26 12:41:31 -0700

    Changes to main.py

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def change_mac(interface, new_mac):
 def get_current_mac(interface):
 	ifconfig_result = subprocess.check_output(["ifconfig", interface])
 	mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
-
     if mac_address_search_result:
 		return mac_address_search_result.group(0)
 	else:
